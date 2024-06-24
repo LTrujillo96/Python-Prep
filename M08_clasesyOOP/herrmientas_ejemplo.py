6b54033135d8f9447241adf6c9b76f0723a41393
LTrujillo96 <luisvargas.trujillo@gmail.com>
2024-06-24 20:25:40 +1000

    tarea realizada

---
+++ M08_clasesyOOP/herrmientas_ejemplo.py
@@ -0,0 +1,88 @@
+class Herramientas:
+    def __init__(self, lista_numeros):
+        self.lista= lista_numeros
+        
+
+    def verifica_primo(self):
+        for i in self.lista:
+            if (self._verifica_primo(i)):
+                print('El elemento', i, 'Si es un numero primo')
+            else:
+                print('El elemento', i, 'No es un numero primo')
+    def conversion_grados(self, origen, destino):
+        for i in self.lista:
+            print(i, 'Grados', origen, 'son', self._conversion_grados(i,origen,destino), 'grados', destino)
+
+    def factorial(self):
+        for i in self.lista:
+            print('El factorial de ', i, 'es', self._factorial(i))
+
+    def _verifica_primo(self, nro):
+        es_primo = True
+        for i in range(2, nro):
+            if nro % i == 0:
+                es_primo = False
+                break
+        return es_primo
+
+    def valor_modal(self,menor):
+        lista_unicos = []
+        lista_repeticiones = []
+        if len(self.lista) == 0:
+            return None
+        for elemento in self.lista:
+            if elemento in lista_unicos:
+                i = lista_unicos.index(elemento)
+                lista_repeticiones[i] += 1
+            else:
+                lista_unicos.append(elemento)
+                lista_repeticiones.append(1)
+        moda = lista_unicos[0]
+        maximo = lista_repeticiones[0]
+        for i, elemento in enumerate(lista_unicos):
+            if lista_repeticiones[i] > maximo:
+                moda = lista_unicos[i]
+                maximo = lista_repeticiones[i]
+        return moda, maximo
+
+    def _conversion_grados(self, valor, origen, destino):
+        valor_destino = None
+        if (origen == 'celsius'):
+            if (destino == 'celsius'):
+                valor_destino = valor
+            elif (destino == 'farenheit'):
+                valor_destino = (valor * 9 / 5) + 32
+            elif (destino == 'kelvin'):
+                valor_destino = valor + 273.15
+            else:
+                print('Parámetro de Destino incorrecto')
+        elif (origen == 'farenheit'):
+            if (destino == 'celsius'):
+                valor_destino = (valor - 32) * 5 / 9
+            elif (destino == 'farenheit'):
+                valor_destino = valor
+            elif (destino == 'kelvin'):
+                valor_destino = ((valor - 32) * 5 / 9) + 273.15
+            else:
+                print('Parámetro de Destino incorrecto')
+        elif (origen == 'kelvin'):
+            if (destino == 'celsius'):
+                valor_destino = valor - 273.15
+            elif (destino == 'farenheit'):
+                valor_destino = ((valor - 273.15) * 9 / 5) + 32
+            elif (destino == 'kelvin'):
+                valor_destino = valor
+            else:
+                print('Parámetro de Destino incorrecto')
+        else:
+            print('Parámetro de Origen incorrecto')
+        return valor_destino
+
+    def _factorial(self, numero):
+        if(type(numero) != int):
+            return 'El numero debe ser un entero'
+        if(numero < 0):
+            return 'El numero debe ser pisitivo'
+        if (numero > 1):
+            numero = numero * self.factorial(numero - 1)
+        return numero
